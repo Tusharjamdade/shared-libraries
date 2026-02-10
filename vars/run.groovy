53200a707ae96cbab174d667be96235264d627a1
Tushar Jamdade <tusharnjamdade@gmail.com>
2026-02-11 00:25:58 +0530

    Create run.groovy

---
+++ vars/run.groovy
@@ -0,0 +1,10 @@
+def call(){
+   echo "Running Container..."
+
+                withCredentials([file(credentialsId: 'ticket-system-env', variable: 'ENV_FILE')]) {
+                    sh '''
+                      cp $ENV_FILE .env
+                      docker compose down || true
+                      docker compose up -d --build
+                    '''
+}
